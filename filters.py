a7441d05b7cddffa178c866b10cc09255754a477
Anton Alekseevich <AntonBoch1244@users.noreply.github.com>
2017-07-19 13:49:44 +0400

    Decolorizer added.

--- filters.py
+++ filters.py
@@ -80,6 +80,58 @@ class filters():
 			col.append(self.color[3])
 		self.color = (col[0], col[1], col[2], col[3])
 	
+	def decolorize(self, color):
+		
+		if type(color) == type(""):
+			if color[0] == "#":
+				color = color.replace("#", "")
+			if color.__len__() == 6:
+				color = (color[:2], color[2:2], color[4:2])
+			elif color.__len__() == 8:
+				color = (color[:2], color[2:2], color[4:2], color[6:2])
+			else:
+				raise ValueError("If are you using string you must use format RRGGBB or RRGGBBAA with or without #")
+		elif type(color) == type([]):
+			col = []
+			for clr in color:
+				if type(clr) == type(0):
+					if clr > 255:
+						clr = 255
+					col.append(str(hex(clr)).replace("0x", ""))
+				elif type(clr) == type(True):
+					if clr:
+						clr = 255
+					else:
+						clr = 0
+					clr = str(hex(clr)).replace("0x", "")
+					if clr.__len__() < 2:
+						clr = "0" + clr
+					col.append(clr)
+				elif type(clr) == type(""):
+					col.append(clr)
+				else:
+					raise ValueError(
+						"If are you using list you must use format ['RR', 'GG', 'BB'] or ['RR', 'GG', 'BB', 'AA'] or in numerical equivalent or boolean.")
+			if col.__len__() == 3:
+				color = (col[0], col[1], col[2])
+			elif col.__len__() == 4:
+				color = (col[0], col[1], col[2], col[3])
+		elif type(color) == type(()):
+			color = color
+		else:
+			raise ValueError("You must use dict or string or list.")
+		index = 0
+		col = []
+		for clr in color:
+			if clr == '':
+				clr = '00'
+			col.append(str(hex(int("0x" + self.color[index], 16) - int("0x" + clr, 16))).replace("0x", ""))
+		try:
+			col[3]
+		except:
+			col.append(self.color[3])
+		self.color = (col[0], col[1], col[2], col[3])
+	
 	def __str__(self):
 		clr = "#"
 		for cll in self.color:
